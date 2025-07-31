2eb11273c88c5d7af9e053381cfbe3995919fe04
yuvrajsinh2209 <136054522+yuvrajsinh2209@users.noreply.github.com>
2025-07-31 11:41:48 +0530

    Create main.tsx

---
+++ client/src/main.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const App = () => {
+  return <h1>Hello from Medtime Vite + React!</h1>;
+};
+
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
